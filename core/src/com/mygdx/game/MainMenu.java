060ae101e9f8c70a92fd441085d1adb720cd11f7
Youken Ikeda <vision573@gmail.com>
2017-01-01 15:40:01 -0500

    Background image in Main Menu Screen

--- core/src/com/mygdx/game/MainMenu.java
+++ core/src/com/mygdx/game/MainMenu.java
@@ -2,27 +2,62 @@ package com.mygdx.game;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.Camera;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.PerspectiveCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
 
 public class MainMenu implements Screen
 {
+    private Viewport viewport;
+    private Camera camera;
+
+    private static final int EXIT_BUTTON_WIDTH = 250;
+    private static final int EXIT_BUTTON_HEIGHT = 120;
+    private static final int PLAY_BUTTON_WIDTH = 300;
+    private static final int PLAY_BUTTON_HEIGHT = 120;
+    private static final int EXIT_BUTTON_Y = 100;
+    private static final int PLAY_BUTTON_Y = 230;
+
     MiceAlert game;
+
+    Texture background;
+    Texture playButtonActive;
+    Texture playButtonInactive;
+    Texture exitButtonActive;
+    Texture exitButtonInactive;
 
     public MainMenu(MiceAlert game)
     {
         this.game = game;
+
+        //camera = new PerspectiveCamera();
+        //viewport = new FitViewport(Gdx.graphics.getWidth(), )
+
+        background = new Texture("background.png");
+        playButtonActive = new Texture("play_button_active.png");
+        playButtonInactive = new Texture("play_button_inactive.png");
+        exitButtonActive = new Texture("exit_button_active.png");
+        exitButtonInactive = new Texture("exit_button_inactive.png");
     }
 
     @Override
     public void show()
     {
-
+        
     }
 
     @Override
     public void render(float delta)
     {
-
+        Gdx.gl.glClearColor(1, 0, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        game.batch.begin();
+        game.batch.draw(background, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        game.batch.draw(playButtonActive, 0, 0, PLAY_BUTTON_WIDTH, PLAY_BUTTON_HEIGHT);
+        game.batch.end();
     }
 
     @Override
@@ -52,6 +87,6 @@ public class MainMenu implements Screen
     @Override
     public void dispose()
     {
-
+        background.dispose();
     }
 }
